8d7149a3a83a71ce5047c937e58a786d0d3fdc96
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-14 02:36:43 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,17 @@
 def unsafe?(speed)
-
+  
+  if speed < 40 || speed > 60
+    return false
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
+  
+  (speed < 40 || speed > 60) false : t
 	
 end
 	
